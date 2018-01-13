8ef90693d2bd25bd9c9b7ff1cfc5d8a2c952ca1a
HLifang <hlf_doris@163.com>
2018-01-13 23:12:06 +0800

    change Book component into stateless component

--- src/Book.js
+++ src/Book.js
@@ -1,38 +1,34 @@
-import React,{Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Book extends Component{
-
-    render(){
-        const {book,onChangeBook}=this.props;
-
-        return (
-            <li>
-                <div className="book">
-                    <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                        <div className="book-shelf-changer">
-                            <select defaultValue={book.shelf ? book.shelf : 'none'} onChange={(e)=>onChangeBook(book,e)}>
-                                <option value="none" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                            </select>
-                        </div>
+function Book(props){
+    const { book, onChangeBook } = props;
+    return (
+        <li>
+            <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                    <div className="book-shelf-changer">
+                        <select defaultValue={book.shelf ? book.shelf : 'none'} onChange={(e) => onChangeBook(book, e)}>
+                            <option value="none" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
                     </div>
-                    <div className="book-title">{book.title}</div>
-                    {book.authors && (
-                        <div className="book-authors">
-                            {book.authors.map((author)=>(
-                                <p key={author}>{author}</p>
-                            ))}
-                        </div>
-                    )}
                 </div>
-            </li>
-        )
-    }
+                <div className="book-title">{book.title}</div>
+                {book.authors && (
+                    <div className="book-authors">
+                        {book.authors.map((author) => (
+                            <p key={author}>{author}</p>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </li>
+    )
 }
 
 Book.propTypes={
